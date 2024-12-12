82de565e23c3071f7e971040bdffba96f048c9f3
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-12 21:02:20 +0500

    Add files via upload

---
+++ Conditions2.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number To Check:"))
+print("Number To Be Checked:",number)
+
+if number%2==0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
