832bb75503821f2cd67b233185aaabe3fea6d42e
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-12 20:54:43 +0500

    Add files via upload

---
+++ Coditions1.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Number To Check:"))
+print("Number To Be Checked:",number)
+
+if number>0:
+    print("This is a positive number")
+elif number<0:
+    print("This is a negative number")
+else:
+        print("This is nuetral")
