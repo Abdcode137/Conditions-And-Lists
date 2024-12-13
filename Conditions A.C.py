6d6b53cff762ebcb76a4e6449af8626cc250fa49
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-13 15:18:28 +0500

    Add files via upload

---
+++ Conditions A.C.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number To Check:"))
+print("Number To Be Checked:",number)
+
+if number>0:
+    print("This is a natural number")
+else:
+    print("This is not a natural number")
